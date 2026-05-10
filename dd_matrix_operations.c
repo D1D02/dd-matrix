d4af817e5e4d376f1bb6b0cdeb696e9747734db7
cavallo02 <duallovercouple@gmail.com>
2026-05-10 20:58:48 +0200

    refactor: improved loop executions adding register keyword for better access to loop variable

--- dd_matrix_operations.c
+++ dd_matrix_operations.c
@@ -8,8 +8,8 @@ dd_error dd_init_matrix( dd_matrix * m, matrix_type val )
     if( !m ) return MATRIX_NULL_POINTER;
     if( !val ) val = 0;
 
-    for( matrix_dimension i = 0; i < m->row; ++i )
-        for( matrix_dimension j = 0; j < m->col; ++j )
+    for( register matrix_dimension i = 0; i < m->row; ++i )
+        for( register matrix_dimension j = 0; j < m->col; ++j )
             APE( m, i, j ) = val;
 
     return OK;
@@ -21,8 +21,8 @@ dd_error dd_init_identity_matrix( dd_matrix * m )
 
     if( !m ) return MATRIX_NULL_POINTER;
 
-    for( matrix_dimension i = 0; i < m->row; ++i )
-        for( matrix_dimension j = 0; j < m->col; ++j )
+    for( register matrix_dimension i = 0; i < m->row; ++i )
+        for( register matrix_dimension j = 0; j < m->col; ++j )
         {
 
             if( i == j ){ APE( m, i, j ) = 1; }
@@ -43,8 +43,8 @@ dd_error dd_sum_matrix( dd_matrix * m1, dd_matrix * m2, dd_matrix * r )
         return ILLEGAL_DIMENSION;
 
 
-    for( matrix_dimension i = 0; i < m1->row; ++i )
-        for( matrix_dimension j = 0; j < m1->col; ++j )
+    for( register matrix_dimension i = 0; i < m1->row; ++i )
+        for( register matrix_dimension j = 0; j < m1->col; ++j )
             APE( r, i, j ) = APE( m1, i, j ) + APE( m2, i, j );
 
     return OK;
@@ -59,8 +59,8 @@ dd_error dd_diff_matrix( dd_matrix * m1, dd_matrix * m2, dd_matrix * r )
         return ILLEGAL_DIMENSION;
 
 
-    for( matrix_dimension i = 0; i < m1->row; ++i )
-        for( matrix_dimension j = 0; j < m1->col; ++j )
+    for( register matrix_dimension i = 0; i < m1->row; ++i )
+        for( register matrix_dimension j = 0; j < m1->col; ++j )
             APE( r, i, j ) = APE( m1, i, j ) - APE( m2, i, j );
     
     return OK;
@@ -74,9 +74,9 @@ dd_error dd_mul_matrix( dd_matrix * m1, dd_matrix * m2, dd_matrix * r )
     if( ( m1->col != m2->row ) || ( m1->row != r->row ) || ( m2->col != r->col ) ) 
         return ILLEGAL_DIMENSION;
 
-    for( matrix_dimension i = 0; i < m1->row; ++i )
-        for( matrix_dimension k = 0; k < m1->col; ++k )
-            for( matrix_dimension j = 0; j < m2->col; ++j )
+    for( register matrix_dimension i = 0; i < m1->row; ++i )
+        for( register matrix_dimension k = 0; k < m1->col; ++k )
+            for( register matrix_dimension j = 0; j < m2->col; ++j )
                 APE( r, i, j ) = APE( r, i, j ) + APE( m1, i, k ) * APE( m2, k, j );
 
     return OK;   
@@ -88,8 +88,8 @@ dd_error dd_scal_mul_matrix( dd_matrix * m, matrix_type val )
     if( !m ) return MATRIX_NULL_POINTER; 
     if( !val ) val = 1;
 
-    for( matrix_dimension i = 0; i < m->row; ++i )
-        for( matrix_dimension j = 0; j < m->col; ++j )
+    for( register matrix_dimension i = 0; i < m->row; ++i )
+        for( register matrix_dimension j = 0; j < m->col; ++j )
             APE( m, i, j ) = val * APE( m, i, j );
 
     return OK;   
@@ -102,8 +102,8 @@ dd_error dd_trans_matrix( dd_matrix * m1, dd_matrix * m2 )
     if( ( m1->row != m2->col ) || ( m1->col != m2->row ) )
         return ILLEGAL_DIMENSION;
 
-    for( matrix_dimension i = 0; i < m1->row; ++i )
-        for( matrix_dimension j = 0; j < m1->col; ++j )
+    for( register matrix_dimension i = 0; i < m1->row; ++i )
+        for( register matrix_dimension j = 0; j < m1->col; ++j )
             APE( m2, j, i ) = APE( m1, i, j );
 
     return OK;   
@@ -114,9 +114,9 @@ dd_error dd_trans_matrix( dd_matrix * m1, dd_matrix * m2 )
 dd_error dd_print_matrix( dd_matrix * m ) 
 {
 
-    for( matrix_dimension i = 0; i < m->row; ++i )
+    for( register matrix_dimension i = 0; i < m->row; ++i )
     {    
-        for( matrix_dimension j = 0; j < m->col; ++j )
+        for( register matrix_dimension j = 0; j < m->col; ++j )
         {
             printf( PRI_MATRIX_TYPE " \t",  APE( m, i, j ) );
         }
